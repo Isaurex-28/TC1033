69ec25224a91f3ce1d1902b080f565515a3b53da
Isauro Garza <91391400+Isaurex-28@users.noreply.github.com>
2025-12-04 22:04:40 -0600

    Formato y Comentarios

--- Desechable.h
+++ Desechable.h
@@ -7,14 +7,37 @@
 
 using namespace std;
 
+/*
+* Clase desechable que hereda de la clase Insumo
+*/
+
 class Desechable: public Insumo{
     private:
+    // Atributos específicos para desechable
     string tipo;
     float tamanio;
     
     public:
+    /*
+    * Constructor de desechable que por default deja el tipo en blanco
+    * y la tamaño en 0
+    * 
+    * @param
+    * @return Objeto Desechable
+    */
     Desechable():Insumo(),tipo(""),tamanio(0.0){};
-    Desechable(string id,string name,float price, int quantity, string type, float size): Insumo(id,name,price,quantity){
+    
+    /*
+    * Constructor de desechable
+    * 
+    * @param string id: ID del desechable, string name: nombre del
+    * desechable, float price: costo unitario, int quantity: cantidad 
+    * en el inventario, string type: tipo de desechable, float size:
+    * tamaño del desechable
+    * @return
+    */
+    Desechable(string id,string name,float price, int quantity, string type, 
+               float size): Insumo(id,name,price,quantity){
         tipo = type;
         tamanio = size;
     }
