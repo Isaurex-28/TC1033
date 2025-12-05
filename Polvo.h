055cb6dc045c41ab6d65dd8537877892d61640a8
Isauro Garza <91391400+Isaurex-28@users.noreply.github.com>
2025-12-04 22:15:10 -0600

    Formato y Comentarios
    Added documentation comments to the Polvo class and its methods.

--- Polvo.h
+++ Polvo.h
@@ -7,14 +7,37 @@
 
 using namespace std;
 
+/*
+* Clase polvo que hereda de la clase Insumo
+*/
+
 class Polvo: public Insumo{
     private:
+    // Atributos específicos para polvo
     string marca;
     float peso;
 
     public:
+    /*
+    * Constructor de polvo que por default deja la marca en blanco y el 
+    * peso en 0
+    * 
+    * @param
+    * @return Objeto Polvo
+    */
     Polvo():Insumo(),marca(""),peso(0.0){};
-    Polvo(string id,string name,float price,int quantity,string brand,float weight):Insumo(id,name,price,quantity){
+    
+    /*
+    * Constructor de polvo
+    * 
+    * @param string id: ID del polvo, string name: nombre del
+    * polvo, float price: costo unitario, int quantity: cantidad 
+    * en el inventario, string brand: marca, float weight:
+    * peso del empaque
+    * @return
+    */
+    Polvo(string id,string name,float price,int quantity,string brand,
+          float weight):Insumo(id,name,price,quantity){
         marca = brand;
         peso = weight;
     };
