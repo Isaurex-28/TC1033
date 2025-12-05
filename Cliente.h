5b22003272705b377fd6bc558db23218c71bb7db
Isauro Garza <91391400+Isaurex-28@users.noreply.github.com>
2025-12-04 22:00:24 -0600

    Formato y Comentarios
    Added documentation comments for the Cliente class and its methods.

--- Cliente.h
+++ Cliente.h
@@ -6,23 +6,42 @@
 
 using namespace std;
 
+/*
+* Clase cliente que actuará como otro de los componentes de la
+* cafetería
+*/
+
 class Cliente{
     private:
+    // Atributos de los clientes
     string nombre;
     string correo;
     bool alergias;
     public:
     
-    //Constructor
+    /*
+    * Constructor de cliente que por default deja el nombre y el correo
+    * en blanco, y las alergias en falso.
+    * 
+    * @param
+    * @return Objeto Cafeteria
+    */
     Cliente():nombre(""),correo(""),alergias(false){};
-    Cliente(string name,string mail, bool allergy):nombre(name), correo(mail), alergias(allergy){};
+    
+    /*
+    * Constructor de cliente
+    * 
+    * @param string string name: nombre del cliente, string mail:
+    * correo electrónico, bool allergy tiene alergias?
+    * @return
+    */    
+    Cliente(string name,string mail, bool allergy):
+        nombre(name), correo(mail), alergias(allergy){};
 
-    //Setters
     void setNombre(string);
     void setCorreo(string);
     void setAlergias(bool);
 
-    //Getters
     string getNombre();
     string getCorreo();
     bool getAlergias();
